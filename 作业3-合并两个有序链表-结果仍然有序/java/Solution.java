5da5f8146162f707b3abe0a704e6008c9f7f9bd3
xubaoguo <xubaoguo196@11.com>
2020-03-05 11:45:11 +0800

    合并两个有序链表结果也有序

---
+++ 作业3-合并两个有序链表-结果仍然有序/java/Solution.java
@@ -0,0 +1,44 @@
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
+
+public class Solution {
+	public void main(int[] args) {
+
+	}
+
+	public ListNode mergeTwoListIteration(ListNode list1, ListNode list2) {
+		ListNode preHead = new ListNode(-1);
+
+		ListNode prev = preHead;
+		while (list1 != null && list2 != null) {
+			if (list1.val < list2.val) {
+				prev.next = list1.next;
+				list1 = list1.nex;
+			} else {
+				prev.next = list2.next;
+				list2 = list2.next;
+			}
+			prev = prev.next;
+		}
+
+		preHead.next = list1 == null ? list2 : list1;
+		return preHead.next;
+	}
+
+	public ListNode mergeTwoListRecursion(ListNode list1, ListNode list2) {
+		if (list1 == null) {
+			return list2;
+		} else if (list2 == null) {
+			return list1;
+		} else (list1.val  < list2.val) {
+			list1.next = mergeTwoListRecursion(list1.next, list2);
+			return list1;
+		} else {
+			list2.next = mergeTwoListRecursion(list1, list2.next);
+			return list2;
+		}
+	}
+}
